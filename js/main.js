fa9e0547bff8607b5e83ef32e93f12bc0b2ef54f
rbHACKer <attensiontorituraj@gmail.com>
2021-03-23 08:27:36 +0530

    committed

--- js/main.js
+++ js/main.js
@@ -13,9 +13,52 @@ VANTA.DOTS({
     backgroundColor: 0x0
 });
 
+// ------------ Preloader --------------------- //
 const loader = document.querySelector(".loader");
+const loaderSVG = document.getElementById("loader-svg");
+const mainTitle = document.querySelector(".demo-title");
 
 setTimeout(() => {
     loader.style.display = "none";
+    loaderSVG.style.display = "none"
     document.body.style.overflowY = "auto";
-}, 5000);
+    mainTitle.style.display = "block";
+}, 3000);
+
+// --------------- Aos initialization -------------- //
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+// search box
+const searchBox = document.querySelectorAll(".search-box input[type='text'] + a");
+
+searchBox.forEach(ele =>{
+    ele.addEventListener("click", () =>{
+        ele.previousElementSibling.value = "";
+    });
+});
